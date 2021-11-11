9017e1741f0bc39eb4cd1a573209198815880d89
karam084 <karam.ezz84@gmail.com>
2021-11-11 18:13:42 +0200

    Fix linter errors

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,68 +1,34 @@
-/*const form = document.getElementById('contact-form');
-
-const validateEmail = (email) => {
-  var emailRegex = /^[a-z]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
-  if (!emailRegex.test(email)) {
-    return false;
-  } else {
-    return true;
-  }
-};
-form.addEventListener('submit', (event) => {
-  // stop form submission
-  event.preventDefault();
-
-  const email = form.elements['email'];
-  if (validateEmail(email)) {
-    email.setCustomValidity(
-      'Please enter a valid email address and in lowercase letters.'
-    );
-
-    //display error message here
-  } else {
-    email.setCustomValidity('');
-    form.submit();
-  }
-});*/
-
-//const form = document.getElementById('contact-form');
-/*form.addEventListener('submit', (event) => {
-  // stop form submission
-  event.preventDefault();
-});*/
 const form = document.getElementById('contact-form');
-const emailEl = document.querySelector('email');
 const emailError = document.querySelector('span.error');
-console.log(emailError);
-form.addEventListener('submit', (event) => {
-  // prevent the form from submitting
-  event.preventDefault();
-
-  const email = form.elements['email'];
-
-  if(!checkEmail(email)){
-    email.setCustomValidity(
-      'Please enter a valid email address and in lowercase letters.'
-    );
-    emailError.textContent = "Please enter a valid email address and in lowercase letters.";
-  } else {
-     email.setCustomValidity('');
-     emailError.textContent = ''; // Reset the content of the message
-     emailError.className = 'error'; // Reset the visual state of the message
-     form.submit();
-  }
-  console.log(email.checkValidity());
-});
 const isEmailValid = (email) => {
   const regex = /^[a-z]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
   return regex.test(email.value);
 };
 
 const checkEmail = (email) => {
-  //console.log(isEmailValid(email));
+  // console.log(isEmailValid(email));
   let valid = true;
   if (!isEmailValid(email)) {
-      valid = false;
-  } 
+    valid = false;
+  }
   return valid;
 };
+
+form.addEventListener('submit', (event) => {
+  // prevent the form from submitting
+  event.preventDefault();
+
+  const { email } = form.elements;
+
+  if (!checkEmail(email)) {
+    email.setCustomValidity(
+      'Please enter a valid email address and in lowercase letters.',
+    );
+    emailError.textContent = 'Please enter a valid email address and in lowercase letters.';
+  } else {
+    email.setCustomValidity('');
+    emailError.textContent = ''; // Reset the content of the message
+    emailError.className = 'error'; // Reset the visual state of the message
+    form.submit();
+  }
+});
